a638fbf189aaf78e308082e6d6cc4515c2bec608
Conor <conormarco@uchicago.edu>
2020-05-16 16:42:02 -0400

    Done.

--- content_scripts/mozzarellafy.js
+++ content_scripts/mozzarellafy.js
@@ -47,9 +47,12 @@
 
     function constructMozarellaArray() {
         // Hardcoded list to various online images
-        var online_pics = ["https://www.seriouseats.com/recipes/images/2015/10/20151017-pies-vicky-wasik-2.jpg"];
+        var online_pics = ["https://www.seriouseats.com/recipes/images/2015/10/20151017-pies-vicky-wasik-2.jpg",
+                "https://sc01.alicdn.com/kf/HTB15aGSKFXXXXa6XpXXq6xXFXXXE.jpg",
+                "https://media1.tenor.com/images/ba6d6be083fcf65f2c79490c5787e5a0/tenor.gif?itemid=9957392",
+                "https://scontent.fatl1-2.fna.fbcdn.net/v/t1.0-9/48393168_2143158979235103_601825901440663552_n.jpg?_nc_cat=103&_nc_sid=85a577&_nc_ohc=lucywEG_L4QAX_V6ksZ&_nc_ht=scontent.fatl1-2.fna&oh=f0560f40aec6b7406a8e21d582f51c90&oe=5EE42579"];
         // Hardcoded list of picture resources in images/mozarellas
-        var local_pics = ["m1.jpg", "image0.gif"];
+        var local_pics = ["m1.jpg", "img2.jpg", "img3.jpg"];
 
         for(var i = 0; i < local_pics.length; i++) {
             var url = browser.extension.getURL("images/mozzarellas/" + local_pics[i]);
